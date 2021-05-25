4277c292629fe801498ac1670331262ca0c164d3
Ryan Roman <rtroman14@gmail.com>
2021-05-25 09:08:20 -0600

    add test route

---
+++ functions/test.js
@@ -0,0 +1,18 @@
+exports.handler = async (event) => {
+    if (event.httpMethod === "GET") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "POST request only" }),
+        };
+    } else if (event.httpMethod === "POST") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ event }),
+        };
+    } else {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ msg: "Error" }),
+        };
+    }
+};
