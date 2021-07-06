ee6e178814830c8e16a416decae01f6047f3b03f
Ryan Roman <rtroman14@gmail.com>
2021-07-06 13:36:22 -0500

    assign chris pegram, update env

--- functions/utils/pipedrivePerson.js
+++ functions/utils/pipedrivePerson.js
@@ -7,7 +7,7 @@ const slackNotification = require("../../src/slackNotification");
 
 module.exports = async (contact) => {
     try {
-        const user = await Pipedrive.getUser("Ryan Roman"); // !IMPORTANT - CHRIS PEGRAM
+        const user = await Pipedrive.getUser("Chris Pegram"); // !IMPORTANT - CHRIS PEGRAM
 
         let personName = contact.full_name || contact.name;
 
