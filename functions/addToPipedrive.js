c9e982212f49e8bae0f91573021b3bb30a0bed7b
Ryan Roman <rtroman14@gmail.com>
2021-07-06 10:27:31 -0500

    final

--- functions/addToPipedrive.js
+++ functions/addToPipedrive.js
@@ -14,7 +14,7 @@ exports.handler = async (event) => {
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ res }),
+            body: JSON.stringify({ person }),
         };
     } else {
         return {
